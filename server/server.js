ca74d7f12a1b9a1b9e8d88fecc9eeee46f5847a3
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2022-02-23 00:13:14 +0000

    added express app

--- server/server.js
+++ server/server.js
@@ -1,24 +1,14 @@
-import path from 'path';
-import express from "express";
-import devBundle from "./devBundle";
-import template from "../template";
-import mongoose from "mongoose";
 
-const CURRENT_WORKING_DIR = process.cwd();
+// import devBundle from "./devBundle";
+import mongoose from "mongoose";
+import app from "./express";
+
 const PORT = process.env.PORT || 3000;
-const app = express();
-devBundle.compile(app)
 
 const DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/social'
-
+// devBundle.compile(app);
 mongoose.connect(DB_URL).then(function(){
     console.log('Database connection established');
-})
-
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
-
-app.get('/', (req, res) => {
-    res.status(200).send(template())
 })
 
 app.listen(PORT, (err) => {
