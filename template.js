2e10405581baff98bca82498d63a78940251b313
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2022-03-08 23:28:30 +0000

    Added template

--- template.js
+++ template.js
@@ -2,7 +2,8 @@ export default () => `
 <html lang="en">
 <title>Social Media MERN App</title>
 <body>
-<div id="root"><h1>Hello world</h1></div>
+<div id="root">Hello</div>
+<script type="text/javascript" src="/dist/bundle.js"></script>
 </body>
 </html>
 `
